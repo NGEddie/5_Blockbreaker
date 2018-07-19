bc97f1435a87e0bc288815456c3c2af72a6b2571
NGEddie <41394346+NGEddie@users.noreply.github.com>
2018-07-19 21:13:07 +0100

    Initial Commit

---
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex;
+
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings -1)
+        {
+            nextSceneIndex = currentSceneIndex + 1;
+        }
+        else
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
